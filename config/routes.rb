81827dc29de2f3a2983b1d87a00f463fe127a743
yugi.muto <gloryday103034k7@gmail.com>
2019-07-21 13:04:39 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
  
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
+
+  resources :microposts, only: [:create, :destroy]
 end
